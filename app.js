66568c487c9dae0859508110e8dd2df87e8668a5
Andrew Ngo <andrewtruongngo@gmail.com>
2018-12-03 23:30:29 -0800

    Includes updates

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
-const titles = document.getElementsByClassName('title');
+let titles = document.getElementsByClassName('title');
 
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
+console.log(Array.from(titles));
+console.log([titles]);
+
+
+// titles.forEach((title) => console.log(title));
+
+
+// const titles = document.getElementsByClassName('title');
+//
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+//
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
+
+let title = document.querySelector('#button');
+
+title.addEventListener('click', (event) => {
+  event.preventDefault();
+  let newDiv = document.createElement('div');
+  newDiv.textContent = 'Hi im a div!';
+  document.body.appendChild(newDiv);
+})
+
+
+
+title.addEventListener('mouseover', () => {
+  title.style.backgroundColor = 'yellow';
+})
+
+title.addEventListener('mouseout', () => {
+  title.style.backgroundColor = '';
+})
+
+document.body.addEventListener('mouseover', (event) => {
+  // console.log(event.target)
+  event.target.style.backgroundColor = 'blue';
+})
+
+document.body.addEventListener('mouseout', (event) => {
+  // console.log(event.target)
+  event.target.style.backgroundColor = 'yellow';
+})
+
+
+
+function f(x) {
+  x*=2;
+  return function )()
+}
